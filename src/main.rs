fa6267096468d4eb7b0432020503b3a50e122a8e
luojia65 <account@luojia.cc>
2019-09-29 23:49:55 +0800

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,68 @@
-use core::ptr::NonNull;
+use core::ptr::{self, NonNull};
+use core::marker::PhantomData;
 use std::ffi::CStr;
+use std::io;
 
 const SYSFS_ROOT: &str = "/sys/bus/usb/devices\0";
 const USB_PREFIX: &str = "usb";
 
-fn main() {
-    // sysfs usb lookup
+pub fn hubs<'iter>() -> io::Result<Hubs<'iter>> {
     let dir = NonNull::new(
         unsafe { libc::opendir(SYSFS_ROOT.as_ptr() as *const _) }
-    ).expect("Error occurred; use errno");
-    while let Some(entry) = NonNull::new(
-        unsafe { libc::readdir(dir.as_ptr()) }
-    ) {
-        if unsafe { libc::strncmp(
-            USB_PREFIX.as_ptr() as *const _,
-            &entry.as_ref().d_name as *const _,
-            3
-        ) } != 0 {
-            continue;
-        }
-        let name = unsafe { 
-            CStr::from_ptr(&entry.as_ref().d_name as *const _)
-        };
-        println!("{}", name.to_string_lossy());
+    );
+    if let Some(dir) = dir {
+        Ok(Hubs { dir, _lifetime_of_dir: PhantomData })
+    } else {
+        Err(io::Error::last_os_error())
     }
-    unsafe { libc::closedir(dir.as_ptr()) };
+}
+
+pub struct Hubs<'iter> {
+    dir: ptr::NonNull<libc::DIR>,
+    _lifetime_of_dir: PhantomData<&'iter ()>
+}
+
+impl Drop for Hubs<'_> {
+    fn drop(&mut self) {
+        unsafe { libc::closedir(self.dir.as_ptr()) };
+    }
+}
+
+impl<'iter> Iterator for Hubs<'iter> {
+    type Item = Hub;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while let Some(entry) = NonNull::new(
+            unsafe { libc::readdir(self.dir.as_ptr()) }
+        ) {
+            if unsafe { libc::strncmp(
+                USB_PREFIX.as_ptr() as *const _,
+                &entry.as_ref().d_name as *const _,
+                3
+            ) } != 0 {
+                continue;
+            }
+            return Some(Hub { entry })
+        }
+        None
+    }
+}
+
+pub struct Hub {
+    entry: NonNull<libc::dirent>,
+}
+
+impl Hub {
+    pub fn name(&self) -> &'_ CStr {
+        unsafe { 
+            CStr::from_ptr(&self.entry.as_ref().d_name as *const _)
+        }
+    }
+}
+
+fn main() {
+    // sysfs usb lookup
+    for hub in hubs().unwrap() {
+        println!("{}", hub.name().to_str().unwrap())
+    }
 }
