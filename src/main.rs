d98cf5c882c7345685b1c1fbe5c3138950968450
luojia65 <account@luojia.cc>
2019-09-29 23:15:10 +0800

    Init

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use core::ptr::NonNull;
+use std::ffi::CStr;
+
+const SYSFS_ROOT: &str = "/sys/bus/usb/devices\0";
+const USB_PREFIX: &str = "usb";
+
+fn main() {
+    // sysfs usb lookup
+    let dir = NonNull::new(
+        unsafe { libc::opendir(SYSFS_ROOT.as_ptr() as *const _) }
+    ).expect("Error occurred; use errno");
+    while let Some(entry) = NonNull::new(
+        unsafe { libc::readdir(dir.as_ptr()) }
+    ) {
+        if unsafe { libc::strncmp(
+            USB_PREFIX.as_ptr() as *const _,
+            &entry.as_ref().d_name as *const _,
+            3
+        ) } != 0 {
+            continue;
+        }
+        let name = unsafe { 
+            CStr::from_ptr(&entry.as_ref().d_name as *const _)
+        };
+        println!("{}", name.to_string_lossy());
+    }
+}
